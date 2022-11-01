35e7d5411f31c20991a45ef052cc12e3448a99bf
Graeme Paterson <ghpaterson@gmail.com>
2022-11-01 11:51:55 +0000

    student input

--- directory.rb
+++ directory.rb
@@ -1,36 +1,42 @@
+def input_students
+puts "Please enter the names of the students"
+puts "To finish, just hit enter twice"
 
-#Im going to create a hash here 
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddie Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+students = []
+
+name = gets.chomp
+
+		while !name.empty? do
+				students << {name: name, cohort: :november}
+				puts "We now have #{students.count} students"
+				name = gets.chomp
+
+		end
+
+		students
+end
+
 #Im going to create methods here
 
 def header
-		puts "The sudents of the Villain Academy"
-  puts "-------------------"
+	puts "The sudents of the Villain Academy"
+	puts "-------------------"
 end
 
 def print(students)
-		students.each do |student|
-  		puts "#{student[:name]} (#{student[:cohort]} cohort)"
-		end
+	students.each do |student|
+			puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	end
 end
 
 def footer(students)
-		puts "Overall we have #{students.count} great students "
+	puts "Overall we have #{students.count} great students "
 end
 
+
+
 #nothing will happen until I call the methods
+students = input_students
 header
 print(students)
 footer(students)
